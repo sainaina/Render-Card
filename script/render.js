604c3b13c390a4a8348a881a0c100c1c7fff0236
Chim Saina <chimsaina982gmail.com>
2024-05-29 14:45:06 +0700

    Render_Card

---
+++ script/render.js
@@ -0,0 +1,10 @@
+import { cardComponent } from "../component/cardComponent.js";
+import { products } from "../data/products.js";
+
+let renderArea = document.querySelector("#card-area");
+
+products.slice(0,10).map((product) => {
+    renderArea.innerHTML += cardComponent(product);
+});
+
+console.log(products);
